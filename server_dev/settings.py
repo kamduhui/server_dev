b1d48a42821d33ea699cedf631a8d9bbedae2bec
kamduhui <kamduhui@gmail.com>
2022-05-01 22:44:30 +0900

    장고도 접근을 가려서 받는데 일단 모든 호스트 허용하게 변경 아스터로 호스트 추가 ["*"]

--- server_dev/settings.py
+++ server_dev/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+_@ku%uzln#ngjw$@pfkn$(=i#^k7edg)+$7^9n5diynlw86=&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
